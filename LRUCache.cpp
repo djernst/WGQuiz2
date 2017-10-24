098d2617aa995374aee89b3206845e81c79a4f07
djernst <daniel@ernstco.us>
2017-10-24 15:20:19 -0500

    Added some notes and augmented the test cases
    More needs to be done....

--- LRUCache.cpp
+++ LRUCache.cpp
@@ -5,14 +5,27 @@
 // Copyright   : Ernstco
 // Description : LRUCache in C++, Ansi-style
 //============================================================================
+// I believe put() and get() operates in O(1) complexity. Notice that there are no loops iterating of any lists or arrays except
+// in the initializer
+// clear() operates in O(1), constant complexity because the list is held to being less than or equal to m_maxCapacity
+//
+// dumpList is a utility.  It operates in O(1); constant complexity, because no more than m_maxCapacity elements are allowed in the array.
+// dumpKeys() is a utility and operates in O(1). It dumps the contents of the key lookup table and is fixed in size (256 elements)
+//
+// The theory of operation is such that when an item is new, it is added to the front of the itemsList.  If the list is already at
+// capacity, the oldest (last) item is removed and then the new item is added to the front of the itemsList.
+// If an item is being updated (its already in the itemsList) it is first removed, then updated and then added to the front of the itemsList.
+// If an item is being accessed (get) the item is first extracted, then it is deleted from the itemsList and then added back to the front of
+// itemsList. Then the extracted is returned to the caller.
+// By using this method, the more frequently accessed items will bubble to the top of the list, while the less frequently accessed items
+// will be at the bottom of the list.
+// Perhaps using a different (or custom) linked list, this operation could save on the copying of data.
+
 
 #include <iostream>
 #include <list>
 
 using namespace std;
-
-
-
 class cacheHitException: public exception
 {
   virtual const char* what() const throw()
@@ -28,6 +41,7 @@ class indexOutOfRangeException: public exception
     return "index out of range. Must be 0-255";
   }
 };
+
 
 template <typename T>
 class LRUCache{
@@ -103,7 +117,6 @@ public:
 		indexOutOfRangeException idxOOREx;
 
 
-		//retVal=T{};
 		if (key >= 256) throw idxOOREx;
 
 		if (listKeys[key].itemRef != itemsList.end()){
@@ -141,69 +154,58 @@ public:
 			}
 		}
 	};
-	void removeLastItem(int key){
-		LRUItem lruItem = itemsList.back();
-		listKeys[lruItem.keyIndex].itemRef = itemsList.end();
-		itemsList.pop_back();
-
-	};
 };
 
 
 
 int main() {
+
+	// Show that this class works with several types (string, float and int)
+
 	LRUCache<string> myStringCache (2);
 	LRUCache<float> myfloatCache (2);
+	LRUCache<int> myIntCache (4);
 
+	// Minimal test with string
 	myStringCache.put(1,"foo");
 	myStringCache.put(2,"bar");
 	cout << "get(1) " << myStringCache.get(1) << endl;
 	cout << "get(2) " << myStringCache.get(2) << endl;
-
 	try {cout << "get(5) " << myStringCache.get(5) << endl;} catch( const exception& e ) {cout << e.what() << endl;}
-
 	myStringCache.dumpList();
 
-
+	// Minimal test with float
 	myfloatCache.put(1,0.4);
 	myfloatCache.put(2,1.2);
 	cout << "get(1) " << myfloatCache.get(1) << endl;
 	cout << "get(2) " << myfloatCache.get(2) << endl;
 	try {cout << "get(5) " << myfloatCache.get(5) << endl;} catch( const exception& e ) {cout << e.what() << endl;}
 	cout << "get(2) " << myfloatCache.get(2) << endl;
-
 	myfloatCache.dumpList();
 
 
-/*
-	myCache.put(2,2);
-	myCache.put(3,3);
-	myCache.put(4,4);
-	myCache.put(5,5);
-	myCache.put(5,6);
+	// More in-depth test with int
 
-	//myCache.dumpKeys();
-	myCache.dumpList();
-	myCache.put(4,8);
+	myIntCache.put(2,2);
+	myIntCache.put(3,3);
+	myIntCache.put(4,4);
+	myIntCache.put(5,5);
+	myIntCache.put(5,6);
+	myIntCache.dumpList();
 
-	//myCache.removeLastItem(3);
-	//myCache.put(4,55);
+	try {cout << "get(6) " << myIntCache.get(6) << endl;} catch( const exception& e ) {cout << e.what() << endl;}
+	try {cout << "get(5) " << myIntCache.get(5) << endl;} catch( const exception& e ) {cout << e.what() << endl;}
+	try {cout << "get(1) " << myIntCache.get(1) << endl;} catch( const exception& e ) {cout << e.what() << endl;}
+	try {cout << "get(2) " << myIntCache.get(2) << endl;} catch( const exception& e ) {cout << e.what() << endl;}
+	try {cout << "get(3) " << myIntCache.get(3) << endl;} catch( const exception& e ) {cout << e.what() << endl;}
+	try {cout << "get(4) " << myIntCache.get(4) << endl;} catch( const exception& e ) {cout << e.what() << endl;}
+	try {cout << "get(2) " << myIntCache.get(2) << endl;} catch( const exception& e ) {cout << e.what() << endl;}
 
-	//myCache.dumpKeys();
-	myCache.dumpList();
+	myIntCache.dumpList();
 
-	cout << "get(1) " << myCache.get(1) << endl;
-	//myCache.dumpKeys();
-	myCache.dumpList();
-
-	cout << "clearing myCache" << endl;
-
-	myCache.clear();
-	myCache.put(5,5);
+	myIntCache.clear();
 
 
-	myCache.dumpList();
-*/
 	return 0;
 }
 
